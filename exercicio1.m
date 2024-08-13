224c9c253b8c61d536cb47d0b507f8cc293665bb
glahr <gjgl.lahr@gmail.com>
2024-08-12 21:39:01 -0300

    adding .m template

---
+++ exercicio1.m
@@ -0,0 +1,15 @@
+function t = exercicio1(func,xl,xu,v,n_m)
+
+% nao alterar: inicio
+es = 1;
+imax = 20;
+% nao alterar: fim
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% digite seu codigo aqui
+t = 0; % alterar
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+endfunction
