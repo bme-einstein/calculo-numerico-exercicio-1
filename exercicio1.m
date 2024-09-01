c1694b2033bc962c9517faf10ed2698dde50b580
Sofia Campana <soh.campana@gmail.com>
2024-09-01 16:48:46 -0300

    oi

--- exercicio1.m
+++ exercicio1.m
@@ -8,7 +8,42 @@ imax = 20;
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % digite seu codigo aqui
-t = 0; % alterar
+t = 0;
+n_iter = 0; 
+ea = 100;
+t_ant = 0;
+h = 35;
+l = 20;
+
+% Avaliação inicial
+fl = func(xl);
+
+while true
+    t = (xl + xu) / 2;
+    fr = func(t)
+
+    n_iter = n_iter + 1;
+    
+    if t ~= 0
+        ea = abs((t - t_ant)/t) * 100;
+    end
+    
+    test = fl * fr;
+    
+    if test < 0
+        xu = t;
+    elseif test > 0
+    xl = t;
+    else
+        ea = 0;
+    end
+    
+    if ea < es || n_iter >= imax
+        break;
+    end
+end
+
+
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
